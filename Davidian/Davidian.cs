543c310d23c5e76f16edea7096e93f1f95be06b0
shavera <alexshaver@gmail.com>
2015-09-18 20:07:02 -0400

    Trivial KSP plugin

---
+++ Davidian/Davidian.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Davidian
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class Davidian : MonoBehaviour
+    {
+        public void Awake() { }
+        public void Start()
+        {
+            print("This is Davidian startup");
+        }
+        public void Update() { }
+        public void FixedUpdate() { }
+        public void OnDestroy() { }
+    }
+}
