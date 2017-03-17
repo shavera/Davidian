4b637b8fb20ce46c28d18c2c28652db8e23d5aee
Alex Shaver <alexshaver@gmail.com>
2017-03-17 14:10:45 -0400

    Add a trivial 'Orbital' library

---
+++ Davidian/Orbital/Orbit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alex on 3/17/17.
+//
+
+#include "Orbit.h"
---
+++ Davidian/Orbital/Orbit.h
@@ -0,0 +1,14 @@
+//
+// Created by alex on 3/17/17.
+//
+
+#ifndef DAVIDIAN_ORBIT_H
+#define DAVIDIAN_ORBIT_H
+
+
+class Orbit {
+
+};
+
+
+#endif //DAVIDIAN_ORBIT_H
