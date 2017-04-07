f4e0a444205b0287a042681d07c37bb56243f476
Alex Shaver <alexshaver@gmail.com>
2017-04-07 09:02:48 -0400

    Add Global Definitions file to impl
    Define things like mass parameter, state vector.

---
+++ Davidian/Orbital/impl/GlobalDefinitions.h
@@ -0,0 +1,22 @@
+//
+// Created by alex on 4/7/17.
+//
+
+#ifndef DAVIDIAN_GLOBALDEFINITIONS_H
+#define DAVIDIAN_GLOBALDEFINITIONS_H
+
+#include "../CartesianVector.h"
+
+namespace orbital{
+
+struct StateVector{
+  CartesianVector position, velocity;
+};
+
+constexpr double G{6.67408e-11};
+
+inline double massParameter(const double& mass){return G*mass;}
+
+} // namespace orbital
+
+#endif //DAVIDIAN_GLOBALDEFINITIONS_H
