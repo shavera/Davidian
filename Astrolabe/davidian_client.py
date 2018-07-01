b57dfff1bb9a3e69464b2cc44392a946eac08b6a
Alex Shaver <alexshaver@gmail.com>
2018-07-01 17:39:01 -0400

    Attempt to draft a simple client class

--- Astrolabe/davidian_client.py
+++ Astrolabe/davidian_client.py
@@ -1,2 +1,15 @@
-def dummyFn():
+import Engine_pb2_grpc
+
+
+def dummy_fn():
     print("Hello World!")
+
+
+class DavidianClient:
+    def __init__(self, server_address='localhost:12345'):
+        self.channel = grpc.insecure_channel(server_address)
+        self.stub = Engine_pb2_grpc.ServerStub(self.channel)
+
+    def load_file(self, filename: str) -> object:
+        response = self.stub.LoadFile(filename)
+        return response
