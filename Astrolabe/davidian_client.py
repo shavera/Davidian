dffbec2dde4831d86183d2898c4b618c42e60241
Alex Shaver <alexshaver@gmail.com>
2018-07-02 15:01:34 -0400

    Clean up client file a little
    Still trying to sort out how to get this to run

--- Astrolabe/davidian_client.py
+++ Astrolabe/davidian_client.py
@@ -4,15 +4,11 @@ import Engine_pb2
 import Engine_pb2_grpc
 
 
-def dummy_fn():
-    print("Hello World!")
-
-
 class DavidianClient:
     def __init__(self, server_address='localhost:12345'):
         self.channel = grpc.insecure_channel(server_address)
         self.stub = Engine_pb2_grpc.ServerStub(self.channel)
 
-    def load_file(self, filename: str) -> Engine_pb2.LoadResponse:
+    def load_file(self, filename: str) -> object:
         response = self.stub.LoadFile(filename)
         return response
--- Astrolabe/main.py
+++ Astrolabe/main.py
@@ -2,7 +2,6 @@ import davidian_client
 
 
 def main():
-    davidian_client.dummyFn()
     client = davidian_client.DavidianClient()
     client.load_file("TestSystem")
 
