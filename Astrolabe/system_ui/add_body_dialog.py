2cf192ad914f0761a375669876c1922aa541b139
Alex Shaver <alexshaver@gmail.com>
2018-07-11 18:55:10 -0400

    Give AddBodyDialog a signal out for root bodies
    more to come

--- Astrolabe/system_ui/add_body_dialog.py
+++ Astrolabe/system_ui/add_body_dialog.py
@@ -1,15 +1,22 @@
+from PySide2 import QtCore
+from PySide2.QtCore import QFile, QObject
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QGroupBox, QComboBox
-from PySide2.QtCore import QFile
 
 
-class AddBodyDialog:
+class AddBodyDialog(QObject):
+
+    add_root_body = QtCore.Signal(str, float)
+
     def __init__(self):
+        super(AddBodyDialog, self).__init__()
         file = QFile("./system_ui/add_body_dialog.ui")
         file.open(QFile.ReadOnly)
 
         loader = QUiLoader()
         self.dialog = loader.load(file)
+
+        self.dialog.accepted.connect(self.on_accepted)
 
     def set_parent_list(self, parent_list: list):
         if 0 != parent_list.count:
@@ -20,3 +27,7 @@ class AddBodyDialog:
 
     def show(self):
         self.dialog.show()
+
+    def on_accepted(self):
+        pass
+
