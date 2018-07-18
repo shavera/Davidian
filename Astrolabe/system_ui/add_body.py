65c135e875cba67123802e075348885fa486c5d4
Alex Shaver <alexshaver@gmail.com>
2018-07-18 08:28:44 -0400

    Test and implement 'add body' behaviours

--- Astrolabe/system_ui/add_body.py
+++ Astrolabe/system_ui/add_body.py
@@ -4,13 +4,12 @@ from PySide2 import QtCore
 from PySide2.QtCore import QFile, QObject
 from PySide2.QtGui import QDoubleValidator
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QComboBox, QGroupBox, QLineEdit
+from PySide2.QtWidgets import QCheckBox, QComboBox, QDoubleSpinBox, QGroupBox, QLineEdit
 
 from client import Orbital_pb2 as Davidian_orbital
 
 
 class AddBodyDialog(QObject):
-    add_root_body = QtCore.Signal(str, float)
     add_body = QtCore.Signal(Davidian_orbital.Body)
 
     def __init__(self):
@@ -24,18 +23,26 @@ class AddBodyDialog(QObject):
 
         self.mass_edit = self.dialog.findChild(QLineEdit, "massEdit")
         mass_validator = QDoubleValidator(self.mass_edit)
+        mass_validator.setBottom(0)
         self.mass_edit.setValidator(mass_validator)
+
+        semimajor_axis_validator = QDoubleValidator(self.mass_edit)
+        semimajor_axis_validator.setBottom(0)
+        self.semimajor_axis_editor = self.dialog.findChild(QLineEdit, "semiMajorAxisEdit")
+        self.semimajor_axis_editor.setValidator(semimajor_axis_validator)
+
+        self.orbit_group = self.dialog.findChild(QGroupBox, "orbitInfoBox")
+
+        self.parent_box = self.dialog.findChild(QComboBox, "parentBodySelectorBox")
 
         self.dialog.accepted.connect(self.on_accepted)
 
     def set_parent_list(self, parent_list: list):
         """Set the parent selection combobox with a list of potential parent bodies. If this list is empty, then this \
         must be a root object"""
-        orbit_group = self.dialog.findChild(QGroupBox, "orbitInfoBox")
-        orbit_group.setEnabled(0 != len(parent_list))
+        self.orbit_group.setEnabled(0 != len(parent_list))
         if 0 != len(parent_list):
-            parent_box = self.dialog.findChild(QComboBox, "parentBodySelectorBox")
-            parent_box.addItems(parent_list)
+            self.parent_box.addItems(parent_list)
 
     def show(self):
         """Display the dialog"""
@@ -47,7 +54,19 @@ class AddBodyDialog(QObject):
         body = Davidian_orbital.Body()
         body.name = name_edit.text()
         body.mass = float(self.mass_edit.text())
-        body.root_body.SetInParent()
 
-        self.add_root_body.emit(name_edit.text(), float(self.mass_edit.text()))
+        if self.orbit_group.isEnabled():
+            is_celestial = self.dialog.findChild(QCheckBox, "celestialBodyBox").isChecked()
+            body_data = body.celestial_body if is_celestial else body.free_body
+            body_data.parent_body_name = self.parent_box.currentText()
+            orbit = body_data.orbit
+            orbit.semimajor_axis = float(self.dialog.findChild(QLineEdit, "semiMajorAxisEdit").text())
+            orbit.eccentricity = self.dialog.findChild(QDoubleSpinBox, "eccentricitySpinBox").value()
+            orbit.inclination = self.dialog.findChild(QDoubleSpinBox, "inclinationSpinBox").value()
+            orbit.longitude_asc_node = self.dialog.findChild(QDoubleSpinBox, "longitudeAscNodeSpinBox").value()
+            orbit.arg_periapsis = self.dialog.findChild(QDoubleSpinBox, "argOfPeriapsisSpinBox").value()
+            orbit.mean_anomaly_0 = self.dialog.findChild(QDoubleSpinBox, "meanAnomalySpinBox").value()
+        else:
+            body.root_body.SetInParent()
+
         self.add_body.emit(body)
--- Astrolabe/system_ui/test_add_body.py
+++ Astrolabe/system_ui/test_add_body.py
@@ -1,7 +1,8 @@
+import math
 import unittest
 
 from PySide2.QtGui import QDoubleValidator
-from PySide2.QtWidgets import QApplication, QCheckBox, QComboBox, QGroupBox, QLineEdit
+from PySide2.QtWidgets import QApplication, QCheckBox, QComboBox, QDoubleSpinBox, QGroupBox, QLineEdit
 
 from client import Orbital_pb2 as Davidian_orbital
 
@@ -33,24 +34,44 @@ def relative_error(expected, actual) -> float:
 
 
 class AddBodyDialogTest(UsesQApplication):
-    root_body_added = False
-
     def setUp(self):
         super(AddBodyDialogTest, self).setUp()
         self.add_body_dialog = add_body.AddBodyDialog()
         self.dialog = self.add_body_dialog.dialog
+
         self.nameEdit = self.dialog.findChild(QLineEdit, "nameEdit")
+        self.assertIsNotNone(self.nameEdit)
+
         self.massEdit = self.dialog.findChild(QLineEdit, "massEdit")
+        self.assertIsNotNone(self.massEdit)
+
         self.orbit_group = self.dialog.findChild(QGroupBox, "orbitInfoBox")
+        self.assertIsNotNone(self.orbit_group)
+
         self.parent_box = self.dialog.findChild(QComboBox, "parentBodySelectorBox")
+        self.assertIsNotNone(self.parent_box)
+
         self.celestial_body_box = self.dialog.findChild(QCheckBox, "celestialBodyBox")
+        self.assertIsNotNone(self.celestial_body_box)
 
     def test_initial_state(self):
         """Test that any specific details about initial conditions are met"""
-        self.assertFalse(self.orbit_group.isEnabled())
-        self.assertTrue(self.celestial_body_box.isChecked())
-        mass_validator = self.massEdit.validator()
-        self.assertIsInstance(mass_validator, QDoubleValidator)
+        with self.subTest("Orbit edit is disabled by default"):
+            self.assertFalse(self.orbit_group.isEnabled())
+
+        with self.subTest("Celestial Body is default body"):
+            self.assertTrue(self.celestial_body_box.isChecked())
+
+        with self.subTest("Mass edit has double validator with 0 as bottom limit"):
+            _mass_validator = self.massEdit.validator()
+            self.assertIsInstance(_mass_validator, QDoubleValidator)
+            self.assertEqual(0, _mass_validator.bottom())
+
+        with self.subTest("SemiMajor Axis edit has double validator with 0 as bottom limit"):
+            _semimajor_axis_edit = self.dialog.findChild(QLineEdit, "semiMajorAxisEdit")
+            _semimajor_axis_validator = _semimajor_axis_edit.validator()
+            self.assertIsInstance(_semimajor_axis_validator, QDoubleValidator)
+            self.assertEqual(0, _semimajor_axis_validator.bottom())
 
     def test_set_parent_list(self):
         """Test what happens when setting parent lists
@@ -67,59 +88,121 @@ class AddBodyDialogTest(UsesQApplication):
             self.assertEqual(len(parent_list), self.parent_box.count())
             for parent in parent_list:
                 self.assertNotEqual(-1, self.parent_box.findText(parent))
-
-    def test_root_body_data_accepted(self):
-        """Test that when a root body's data is entered, the add_body signal is emitted with the appropriate data"""
-        name = "Kerbol"
-        self.nameEdit.setText(name)
-        mass = "1.7565459e28"
-        self.massEdit.setText(mass)
-
-        add_root_body_called = False
-
-        def _on_add_root_body(actual_name, actual_mass):
-            nonlocal add_root_body_called
-            add_root_body_called = True
-            self.assertEqual(name, actual_name)
-            self.assertLess(relative_error(1.7565459e28, actual_mass), 1e-6)
-
-        self.add_body_dialog.add_root_body.connect(_on_add_root_body)
-
-        self.dialog.accept()
-
-        self.assertTrue(add_root_body_called)
+            self.assertEqual(parent_list[0], self.parent_box.currentText())
 
 
 class AddBodyDialogAcceptedTest(AddBodyDialogTest):
     add_body_emitted = False
     signalled_body = Davidian_orbital.Body()
+    parent_list = ["Kerbol", "Kerbin", "Mun"]
 
     def setUp(self):
         super(AddBodyDialogAcceptedTest, self).setUp()
         self.add_body_dialog.add_body.connect(self._on_add_body)
+
+        self.semimajor_axis_edit = self.dialog.findChild(QLineEdit, "semiMajorAxisEdit")
+        self.assertIsNotNone(self.semimajor_axis_edit)
+
+        self.eccentricity_spin_box = self.dialog.findChild(QDoubleSpinBox, "eccentricitySpinBox")
+        self.assertIsNotNone(self.eccentricity_spin_box)
+
+        self.inclination_spin_box = self.dialog.findChild(QDoubleSpinBox, "inclinationSpinBox")
+        self.assertIsNotNone(self.inclination_spin_box)
+
+        self.longitude_asc_node_spin_box = self.dialog.findChild(QDoubleSpinBox, "longitudeAscNodeSpinBox")
+        self.assertIsNotNone(self.longitude_asc_node_spin_box)
+
+        self.arg_periapsis_spin_box = self.dialog.findChild(QDoubleSpinBox, "argOfPeriapsisSpinBox")
+        self.assertIsNotNone(self.arg_periapsis_spin_box)
+
+        self.mean_anomaly_spin_box = self.dialog.findChild(QDoubleSpinBox, "meanAnomalySpinBox")
+        self.assertIsNotNone(self.mean_anomaly_spin_box)
 
     def _on_add_body(self, body):
         self.add_body_emitted = True
         self.signalled_body = body
 
     def test_root_body(self):
-        name = "Kerbol"
-        self.nameEdit.setText(name)
-        mass = "1.7565459e28"
-        self.massEdit.setText(mass)
+        """Test that when a root body's data is entered, the add_body signal is emitted with the appropriate data"""
+        expected_body = Davidian_orbital.Body()
+        expected_body.name = "Kerbol"
+        expected_body.mass = 1.7565459e28
+        expected_body.root_body.SetInParent()
+        self.nameEdit.setText(expected_body.name)
+        self.massEdit.setText(str(expected_body.mass))
 
         self.dialog.accept()
 
         self.assertTrue(self.add_body_emitted)
-        self.assertEqual(name, self.signalled_body.name)
-        self.assertEqual(float(mass), self.signalled_body.mass)
-        self.assertTrue(self.signalled_body.HasField('root_body'))
-
-    def test_free_body(self):
-        pass
+        self.assertEqual(expected_body, self.signalled_body)
 
     def test_celestial_body(self):
-        pass
+        """Test that when parents are present can enter data for a child celestial body"""
+        self.add_body_dialog.set_parent_list(self.parent_list)
+        parent_index = 1
+        expected_body = Davidian_orbital.Body()
+        expected_body.name = "Minmus"
+        expected_body.mass = 2.6457580e19
+        body_data = expected_body.celestial_body
+        orbit = body_data.orbit
+        body_data.parent_body_name = self.parent_list[parent_index]
+        orbit.semimajor_axis = 4.7e7
+        orbit.eccentricity = 0
+        orbit.inclination = 6.0
+        orbit.longitude_asc_node = 78
+        orbit.arg_periapsis = 38
+        # 0.9 radians, rounded to 2 decimals, is 51.57
+        orbit.mean_anomaly_0 = 51.57
+
+        self.add_body_dialog.set_parent_list(self.parent_list)
+        self.nameEdit.setText(expected_body.name)
+        self.massEdit.setText(str(expected_body.mass))
+        self.parent_box.setCurrentIndex(self.parent_box.findText(body_data.parent_body_name))
+        self.semimajor_axis_edit.setText(str(orbit.semimajor_axis))
+        self.eccentricity_spin_box.setValue(orbit.eccentricity)
+        self.inclination_spin_box.setValue(orbit.inclination)
+        self.longitude_asc_node_spin_box.setValue(orbit.longitude_asc_node)
+        self.arg_periapsis_spin_box.setValue(orbit.arg_periapsis)
+        self.mean_anomaly_spin_box.setValue(orbit.mean_anomaly_0)
+
+        self.dialog.accept()
+
+        self.assertTrue(self.add_body_emitted)
+        self.assertEqual(expected_body, self.signalled_body)
+
+    def test_free_body(self):
+        """Test that when parents are present, can deselect celestial body and enter data for a free body"""
+        parent_index = 1
+        expected_body = Davidian_orbital.Body()
+        expected_body.name = "Ship"
+        expected_body.mass = 200000
+        body_data = expected_body.free_body
+        orbit = body_data.orbit
+        body_data.parent_body_name = self.parent_list[parent_index]
+        orbit.semimajor_axis = 700000
+        orbit.eccentricity = 0.03
+        orbit.inclination = 13.75
+        orbit.longitude_asc_node = -35.23
+        orbit.arg_periapsis = 103.24
+        orbit.mean_anomaly_0 = 36.6
+
+        self.add_body_dialog.set_parent_list(self.parent_list)
+        self.nameEdit.setText(expected_body.name)
+        self.massEdit.setText(str(expected_body.mass))
+        self.parent_box.setCurrentIndex(self.parent_box.findText(body_data.parent_body_name))
+        # remember to remove celestial body check here
+        self.celestial_body_box.setChecked(False)
+        self.semimajor_axis_edit.setText(str(orbit.semimajor_axis))
+        self.eccentricity_spin_box.setValue(orbit.eccentricity)
+        self.inclination_spin_box.setValue(orbit.inclination)
+        self.longitude_asc_node_spin_box.setValue(orbit.longitude_asc_node)
+        self.arg_periapsis_spin_box.setValue(orbit.arg_periapsis)
+        self.mean_anomaly_spin_box.setValue(orbit.mean_anomaly_0)
+
+        self.dialog.accept()
+
+        self.assertTrue(self.add_body_emitted)
+        self.assertEqual(expected_body, self.signalled_body)
 
 
 if __name__ == '__main__':
